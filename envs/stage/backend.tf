7dfdb4e8aee6ac0830e95611021c57d771729295
jpnino1018 <juan.nino@u.icesi.edu.co>
2025-06-12 22:57:22 -0500

    updated remote backend

--- envs/stage/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "ecommerceinfrastrgacc"
-    container_name       = "tfstate"
-    key                  = "stage.terraform.tfstate"
-  }
-}
